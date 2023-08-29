f7e115da6b6c537e8f3c64021464af25122e8532
Maksim Grebenozhko <m.grebenozhko@tinkoff.ru>
2023-08-29 07:25:39 +0400

    Initial Commit

---
+++ TremblingLetters/TremblingLettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TremblingLettersApp.swift
+//  TremblingLetters
+//
+//  Created by m.grebenozhko on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TremblingLettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
