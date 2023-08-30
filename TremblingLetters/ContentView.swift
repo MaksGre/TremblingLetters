4cec4104de6fe97575b6a8a6ee0c81ff7a2a1fc8
Maksim Grebenozhko <m.grebenozhko@tinkoff.ru>
2023-08-30 08:55:26 +0400

    Поле ввода, кнопка

--- TremblingLetters/ContentView.swift
+++ TremblingLetters/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userText = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "textformat.abc")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Введите текст", text: $userText)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            Button("Поехали!") {
+                print("Hello")
+            }.padding(.top)
         }
-        .padding()
     }
 }
 
